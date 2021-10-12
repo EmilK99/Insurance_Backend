aaaec1375a152d10e5a3eeb102959245a26a6f68
drepa-dasha <drepa.dasha@mail.ru>
2021-10-13 01:17:44 +0700

    - Mock and heroku config

--- app/handlers.go
+++ app/handlers.go
@@ -67,11 +67,6 @@ func (s *server) HandleGetPayouts(w http.ResponseWriter, r *http.Request) {
 
 	var req store.GetContractsReq
 
-	type response struct {
-		Contracts   []*store.ContractsInfo `json:"contracts"`
-		TotalPayout float32                `json:"total_payout"`
-	}
-
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil { // bad request
 		w.WriteHeader(400)
@@ -79,22 +74,30 @@ func (s *server) HandleGetPayouts(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	payouts, err := s.store.GetPayouts(s.ctx, req.UserID)
-	if err != nil {
-		w.WriteHeader(422)
-		_ = json.NewEncoder(w).Encode(map[string]string{"code": strconv.Itoa(422), "message": err.Error(), "status": "Error"})
-		return
-	}
+	//payouts, err := s.store.GetPayouts(s.ctx, req.UserID)
+	//if err != nil {
+	//	w.WriteHeader(422)
+	//	_ = json.NewEncoder(w).Encode(map[string]string{"code": strconv.Itoa(422), "message": err.Error(), "status": "Error"})
+	//	return
+	//}
 	var res store.GetPayoutsResponse
+	//
+	//for i := range payouts {
+	//	ctr := store.ContractsInfo{ContractID: payouts[i].ContractId,
+	//		FlightNumber: payouts[i].FlightNumber,
+	//		Status:       "cancelled",
+	//		Reward:       payouts[i].TicketPrice,
+	//	}
+	//	res.Contracts = append(res.Contracts, &ctr)
+	//	res.TotalPayout += payouts[i].TicketPrice
+	//}
 
-	for i := range payouts {
-		ctr := store.ContractsInfo{ContractID: payouts[i].ContractId,
-			FlightNumber: payouts[i].FlightNumber,
-			Status:       "cancelled",
-			Reward:       payouts[i].TicketPrice,
+	res.Contracts = ContractsMock
+
+	for i := range ContractsMock {
+		if ContractsMock[i].Status == "cancelled" {
+			res.TotalPayout += ContractsMock[i].Reward
 		}
-		res.Contracts = append(res.Contracts, &ctr)
-		res.TotalPayout += payouts[i].TicketPrice
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
